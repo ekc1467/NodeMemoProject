54d68ea8e0d3f26e7c0f0540dc2ec9da7a8dad3a
ekc1467 <ekc1467@naver.com>
2022-05-06 14:52:14 +0900

    jjj

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Route } from "react-router-dom";
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import Start from './Start';
 import Check from './Check';
 import MemoList from './Memo/MemoList';
@@ -12,7 +12,7 @@ class App extends Component{
     render (){
         return(
         <div className = "App">
-            <Route exact path ='/' component = {Check}/>
+            <Route exact path ='/' component = {Start}/>
             <Route path ='/MemoList' component = {MemoList}/>
             <Route path ='/MemoShow' component = {MemoShow}/>
         </div>
